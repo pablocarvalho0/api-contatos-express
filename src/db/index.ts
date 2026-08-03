3bd61e3182c7130c1609fad680292580bfbe2ba6
Pablo Carvalho <pablocnsantos@gmail.com>
2026-08-03 16:17:35 -0300

    feat: initialize Drizzle ORM configuration and database connection setup

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+export const db = drizzle({
+    connection: {
+        connectionString: process.env.DATABASE_URL as string,
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000
+    }
+});
