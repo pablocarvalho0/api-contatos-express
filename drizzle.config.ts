b5e23cb754ef5f01cd9480a496afa5c6f3e57e73
Pablo Carvalho <pablocnsantos@gmail.com>
2026-08-03 16:18:51 -0300

    chore: reorder drizzle config properties and enable verbose and strict modes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-// import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './drizzle',
-    schema: './src/db/schema.ts',
     dialect: 'postgresql',
+    schema: './src/db/schema.ts',
+    out: './drizzle',
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
+        url: process.env.DATABASE_URL as string,
     },
+    verbose: true,
+    strict: true,
 });
